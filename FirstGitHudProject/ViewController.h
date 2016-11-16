3c8b12cc6496156a6b8789dad5493ec44bddef30
qidongqiang <ZHUBINGFENG@qidongqiangdemac-mini.local>
2016-11-16 13:52:33 +0800

    Initial Commit

---
+++ FirstGitHudProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstGitHudProject
+//
+//  Created by ZHUBINGFENG on 16/11/16.
+//  Copyright © 2016年 xinxidacheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstGitHudProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstGitHudProject
+//
+//  Created by ZHUBINGFENG on 16/11/16.
+//  Copyright © 2016年 xinxidacheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
